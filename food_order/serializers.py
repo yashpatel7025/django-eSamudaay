8fe7cf941f3266048662a99bb8c02d81a5c4701b
Yash Patel <yashpatel@Yashs-MacBook-Pro.local>
2021-06-26 15:21:57 +0530

    few validations added

--- food_order/serializers.py
+++ food_order/serializers.py
@@ -7,11 +7,15 @@ class OrderItemSerializer(serializers.Serializer):
 
 class OfferSerializer(serializers.Serializer):
   offer_type = serializers.CharField(required=True)
-  offer_val = serializers.IntegerField(required=True)
+  offer_val = serializers.IntegerField(required=False)
 
   def validate(self, data):
     if data["offer_type"] not in ["FLAT","DELIVERY"]:
-      raise serializers.ValidationError("offer type can be FLAT or DELIVERY")
+      raise serializers.ValidationError("offer_type can be FLAT or DELIVERY")
+
+    if data["offer_type"] == "FLAT" and not data.get("offer_val"):
+      raise serializers.ValidationError("offer_val can not be null for FLAT offer")
+
     return data
 
 class EntityWithIdSerializer(serializers.Serializer):
